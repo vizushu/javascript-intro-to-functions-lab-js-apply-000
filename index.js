bf8d5bd9524952d395b56fce8e40416a651004ab
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-04 02:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,15 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if (string == string.toLowerCase()) return 'I can\'t hear you!'
   else if (string == string.toUpperCase()) return 'YES INDEED!'
   else if (string == 'I love you, Grandma.') return 'I love you, too.'
+=======
+  if (string === string.toLowerCase()) return 'I can\'t hear you!'
+  else if (string === string.toUpperCase()) return 'YES INDEED!'
+  else if (string === "I love you, Grandma.") return "I love you, too.";
+>>>>>>> 6fb29c47219be20127db14b391da7df37e27f826
 }
 sayHiToGrandma('hello')
 sayHiToGrandma('HELLO')
